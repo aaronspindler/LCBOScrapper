38f75edd4dd3e0f85c008d93bd5bfe6981f95645
xNovax <aaron@xnovax.net>
2016-07-29 17:58:50 -0400

    First push

---
+++ LCBO_Scraper/Page.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCBO_Scraper
+{
+
+    public class Page
+    {
+        public int status { get; set; }
+        public object message { get; set; }
+        public Pager pager { get; set; }
+        public Result[] result { get; set; }
+        public object suggestion { get; set; }
+    }
+
+    public class Pager
+    {
+        public int records_per_page { get; set; }
+        public int total_record_count { get; set; }
+        public int current_page_record_count { get; set; }
+        public bool is_first_page { get; set; }
+        public bool is_final_page { get; set; }
+        public int current_page { get; set; }
+        public string current_page_path { get; set; }
+        public int next_page { get; set; }
+        public string next_page_path { get; set; }
+        public object previous_page { get; set; }
+        public object previous_page_path { get; set; }
+        public int total_pages { get; set; }
+        public string total_pages_path { get; set; }
+    }
+
+    public class Result
+    {
+        public int id { get; set; }
+        public bool is_dead { get; set; }
+        public string name { get; set; }
+        public string tags { get; set; }
+        public bool is_discontinued { get; set; }
+        public int price_in_cents { get; set; }
+        public int regular_price_in_cents { get; set; }
+        public int limited_time_offer_savings_in_cents { get; set; }
+        public string limited_time_offer_ends_on { get; set; }
+        public int bonus_reward_miles { get; set; }
+        public string bonus_reward_miles_ends_on { get; set; }
+        public string stock_type { get; set; }
+        public string primary_category { get; set; }
+        public string secondary_category { get; set; }
+        public string origin { get; set; }
+        public string package { get; set; }
+        public string package_unit_type { get; set; }
+        public int package_unit_volume_in_milliliters { get; set; }
+        public int total_package_units { get; set; }
+        public int volume_in_milliliters { get; set; }
+        public int alcohol_content { get; set; }
+        public int price_per_liter_of_alcohol_in_cents { get; set; }
+        public int price_per_liter_in_cents { get; set; }
+        public int inventory_count { get; set; }
+        public int inventory_volume_in_milliliters { get; set; }
+        public int inventory_price_in_cents { get; set; }
+        public object sugar_content { get; set; }
+        public string producer_name { get; set; }
+        public string released_on { get; set; }
+        public bool has_value_added_promotion { get; set; }
+        public bool has_limited_time_offer { get; set; }
+        public bool has_bonus_reward_miles { get; set; }
+        public bool is_seasonal { get; set; }
+        public bool is_vqa { get; set; }
+        public bool is_ocb { get; set; }
+        public bool is_kosher { get; set; }
+        public string value_added_promotion_description { get; set; }
+        public object description { get; set; }
+        public string serving_suggestion { get; set; }
+        public string tasting_note { get; set; }
+        public DateTime updated_at { get; set; }
+        public string image_thumb_url { get; set; }
+        public string image_url { get; set; }
+        public string varietal { get; set; }
+        public string style { get; set; }
+        public string tertiary_category { get; set; }
+        public int? sugar_in_grams_per_liter { get; set; }
+        public int clearance_sale_savings_in_cents { get; set; }
+        public bool has_clearance_sale { get; set; }
+        public int product_no { get; set; }
+    }
+
+}
---
+++ LCBO_Scraper/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace LCBO_Scraper
+{
+    class Program
+    {
+        static int pageNum = 1;
+        static int pageLimit = 999999;
+        static List<Page> pages = new List<Page>();
+        static List<Result> products = new List<Result>();
+
+        static public double getMlPerDollar(Result r)
+        {
+            double ml = r.volume_in_milliliters/1000;
+            double per = r.alcohol_content / 100;
+            double price = r.price_in_cents / 100;
+
+            return (ml * per) / price;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to LCBO Scrapper!");
+
+            var wc = new WebClient();
+            try
+            {
+                while (pageNum < pageLimit)
+                {
+                    var url = ("https://lcboapi.com/products?page=" + pageNum + "&access_key=MDpmZDNhMGM5Yy0yOGZmLTExZTYtYTdmNS03Nzk2ZTY1MTg4Y2U6TVZtTzFFdUpPZFhUaDNJYnV0Q1E2NmlxdUtTVk5WUWFsUjlO");
+                    string downloadedData = wc.DownloadString(url);
+
+                    try
+                    {
+                        Page currentPage = Newtonsoft.Json.JsonConvert.DeserializeObject<Page>(downloadedData);
+                        if (pageNum == 1)
+                        {
+                            pageLimit = currentPage.pager.total_pages;
+                        }
+
+                        for (int i = 0; i < currentPage.result.Length; i++)
+                        {
+                            Console.WriteLine("Name: " + currentPage.result[i].name + " - " + getMlPerDollar(currentPage.result[i]));
+                            products.Add(currentPage.result[i]);
+                        }
+                    }
+                    catch(Exception e) { }
+                    pageNum++;
+                }
+            }
+            catch (Exception webError)
+            {
+                Console.WriteLine("Error Message: " + webError.Message);
+            }
+
+            Console.WriteLine("Number of products: " + products.Count);
+
+            StreamWriter writer = new StreamWriter("file.csv");
+
+            for(int i = 0; i < products.Count; i++)
+            {
+                writer.WriteLine(products[i].name + ";" + getMlPerDollar(products[i]) + "ml");
+            }
+
+            Result bestProduct = products[0];
+            for(int i = 1; i < products.Count; i++)
+            {
+                Result curResult = products[i];
+
+                if(getMlPerDollar(curResult) > getMlPerDollar(bestProduct))
+                {
+                    if (curResult.is_dead) continue;
+                    if (curResult.is_discontinued) continue;
+                    if (curResult.price_per_liter_of_alcohol_in_cents < 10) continue;
+                    if (curResult.varietal == "Fortified Wine") continue;
+                    if (curResult.varietal == "Aperitif Wine") continue;
+
+                    bestProduct = curResult;
+                }
+            }
+
+            Console.WriteLine("Name: " + bestProduct.name + " Percentage: " + bestProduct.alcohol_content / 100 + " Price: " + bestProduct.regular_price_in_cents / 100);
+
+            Console.WriteLine("Press enter to close");
+            Console.ReadLine();
+
+        }
+    }
+}
